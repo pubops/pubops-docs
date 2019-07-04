d8a09f3c183c0e30d602d50282ba7684be0eb9e7
Denis Pshenov <denis.pshenov@gmail.com>
2019-07-04 19:30:42 +0700

    Add favicon and adjust header logo

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -20,9 +20,9 @@ const siteConfig = {
   ],
 
   /* path to images for header/footer */
-  // headerIcon: 'img/favicon.ico',
-  // footerIcon: 'img/favicon.ico',
-  // favicon: 'img/favicon.ico',
+  headerIcon: 'img/logo-light.png',
+  // footerIcon: 'favicon.ico',
+  favicon: 'favicon.ico',
 
   /* Colors for website */
   colors: {
