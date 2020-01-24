52d71663045a1021b6e9f0482c85468a2cafd9fc
Denis Pshenov <denis.pshenov@gmail.com>
2020-01-24 17:04:02 +0700

    Add TOC to documents

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -58,7 +58,7 @@ const siteConfig = {
   // scripts: ['https://buttons.github.io/buttons.js'],
 
   // On page navigation for the current documentation page.
-  // onPageNav: 'separate',
+  onPageNav: 'separate',
   
   // No .html extensions for paths.
   cleanUrl: true,
